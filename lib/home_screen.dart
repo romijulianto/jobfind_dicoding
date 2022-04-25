7193bfde6bda6701bc3b0a69a4192c1b1e658fa9
romijulianto <romyjulians@gmail.com>
2022-04-25 22:09:46 +0700

    Revisi Submission 3

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,24 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:jobfind_dicoding/drawer_screen.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   final String value;
-  HomeScreen({
+  const HomeScreen({
     Key? key,
     required String this.value,
   }) : super(key: key);
 
-  @override
-  _HomescreenState createState() => _HomescreenState();
-}
-
-class _HomescreenState extends State<HomeScreen> {
-  _HomescreenState();
-  
-  @override
-  void initState() {
-    super.initState();
-  }
 
   //
   @override
